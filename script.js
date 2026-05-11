09870c217d0ee172104c30360009337716ba1af3
ravichandra-5859 <chandrapathi5859@gmail.com>
2026-05-11 16:41:48 +0530

    Refactor functions to use window.onload for initialization

--- script.js
+++ script.js
@@ -1,44 +1,29 @@
- function T1() {
-        let a = new Date();
-        window.alert(a.getFullYear());
-      }
-      function T2() {
-        let b = new Date();
-        window.alert(b.getMonth());
-      }
-      function T3() {
-        let c = new Date();
-        window.alert(c.getDay());
-      }
-      function T4() {
-        let d = new Date();
-        window.alert(d.getHours());
-      }
-      function T5() {
-        let e = new Date();
-        window.alert(e.getMinutes());
-      }
+window.onload = function () {
 
-      var a = new Date();
-      document.write(a.get());
-      let display = document.getElementById("display");
-      // Add value to display
-      function addValue(value) {
-        display.value = display.value + value;
-      }
-      // Clear display
-      function clearDisplay() {
-        display.value = "";
-      }
-      // Delete last character
-      function deleteLast() {
-        display.value = display.value.slice(0, -1);
-      }
-      // Calculate result
-      function calculate() {
-        try {
-          display.value = eval(display.value);
-        } catch (error) {
-          display.value = "Error";
-        }
-      }
+  let display = document.getElementById("display");
+
+  // Add value
+  window.addValue = function(value) {
+    display.value += value;
+  };
+
+  // Clear display
+  window.clearDisplay = function() {
+    display.value = "";
+  };
+
+  // Delete last character
+  window.deleteLast = function() {
+    display.value = display.value.slice(0, -1);
+  };
+
+  // Calculate result
+  window.calculate = function() {
+    try {
+      display.value = eval(display.value);
+    } catch {
+      display.value = "Error";
+    }
+  };
+
+};
